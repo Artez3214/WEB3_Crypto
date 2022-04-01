bf1ab19b0b8fbfc4017eb7adbc5fb85b633eaca9
Eimantas Garncevas <eimantas.garncevas@stud.vilniustech.lt>
2022-04-01 21:06:19 +0300

    Projekto pirmi failai

---
+++ client/src/App.jsx
@@ -0,0 +1,17 @@
+
+const App = () => {
+
+
+  return (
+    <div className="App">
+     <h1 class="text-3xl font-bold underline">
+    Hello world!
+  </h1>
+  <button class="bg-sky-600 hover:bg-sky-700 ...">
+  Save changes
+</button>
+    </div>
+  )
+}
+
+export default App
