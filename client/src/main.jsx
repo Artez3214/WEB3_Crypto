9cee7a8d0d2030af477a474190199200a50e69e7
Eimantas Garncevas <eimantas.garncevas@stud.vilniustech.lt>
2022-04-03 22:59:29 +0300

    sutvarkiau failus kad veiktu tailwind

--- client/src/main.jsx
+++ client/src/main.jsx
@@ -1,9 +1,7 @@
-import React from "react";
-import ReactDOM from "react-dom";
-
-import App from "./App";
-//import { TransactionsProvider } from "./context/TransactionContext";
-import "./index.css";
+import React from 'react'
+import ReactDOM from 'react-dom'
+import './index.css'
+import App from './App'
 
 ReactDOM.render(
   <React.StrictMode>
